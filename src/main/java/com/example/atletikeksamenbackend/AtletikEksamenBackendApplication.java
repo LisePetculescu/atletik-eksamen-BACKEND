dd03528cdb2761c92682c6f744d0375d0c297783
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-06-20 13:05:02 +0200

    init commit

---
+++ src/main/java/com/example/atletikeksamenbackend/AtletikEksamenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.atletikeksamenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtletikEksamenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtletikEksamenBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/atletikeksamenbackend/AtletikEksamenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atletikeksamenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtletikEksamenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
